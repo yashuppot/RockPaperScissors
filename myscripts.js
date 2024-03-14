bb9449726bce0503e4b5dd77b645b0f83b7a6cc3
Yash Uppot <yashuppot@gmail.com>
2024-03-14 04:05:50 -0400

    Implemented playRound

--- myscripts.js
+++ myscripts.js
@@ -12,3 +12,45 @@ function getComputerChoice(){
             break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toUpperCase() === "ROCK") {
+        switch (computerSelection) {
+            case "Rock":
+                console.log("You tied! Rock ties Rock");
+                break;
+            case "Paper":
+                console.log("You lost! Paper beats Rock");
+                break;
+            case "Scissors":
+                console.log("You won! Rock beats Scissors");
+                break;
+        }
+    }
+    else if (playerSelection.toUpperCase() === "PAPER") {
+        switch (computerSelection) {
+            case "Rock":
+                console.log("You won! Paper beats Rock");
+                break;
+            case "Paper":
+                console.log("You tied! Paper ties Paper");
+                break;
+            case "Scissors":
+                console.log("You lost! Scissors beats Paper");
+                break;
+        }
+    }
+    else if (playerSelection.toUpperCase() === "SCISSORS") {
+        switch (computerSelection) {
+            case "Rock":
+                console.log("You lost! Rock beats Scissors");
+                break;
+            case "Paper":
+                console.log("You won! Scissors beats Paper");
+                break;
+            case "Scissors":
+                console.log("You tied! Scissors ties Scissors");
+                break;
+        }
+    }       
+}
