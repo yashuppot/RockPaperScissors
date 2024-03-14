946b307a2c75e2615f52f1d89e5a0b71206da842
Yash Uppot <yashuppot@gmail.com>
2024-03-14 03:52:22 -0400

    Implement getComputerChoice

---
+++ myscripts.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+    choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+    }
+}
